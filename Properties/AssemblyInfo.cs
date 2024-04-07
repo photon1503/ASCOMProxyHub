a8046e6e051d72c0f2b3abf9d229e78e73615473
photon1503 <g.hitz@gmx.at>
2024-04-07 21:00:14 +0200

    fixed unpark

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 
 [assembly: AssemblyVersion("6.6.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.6")]
--- TelescopeDriver/TelescopeHardware..cs
+++ TelescopeDriver/TelescopeHardware..cs
@@ -1271,7 +1271,7 @@ namespace ASCOM.photonProxyHub.Telescope
         /// </summary>
         internal static void Unpark()
         {
-            if (driver.AtPark)
+           // if (driver.AtPark)
                 _AtPark = false;
                 driver.Unpark();
 
